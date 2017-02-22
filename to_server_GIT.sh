0b638729aa261445f08c0bd865f9259bca65377e
alessandroskytechnology <marturanoale@gmail.com>
2017-02-22 20:24:38 +0000

    commit_from_the_script

--- to_server_GIT.sh
+++ to_server_GIT.sh
@@ -2,7 +2,7 @@
 #add all of the file into the repository
 git add .
 #commit to the local repository
-git commit -m "this is a comment"
+git commit -m "$1"
 #push into the server
 git push -u origin master
 

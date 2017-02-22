cc5f4568deea13e5ec6f8c438bcff16a02298720
alessandroskytechnology <marturanoale@gmail.com>
2017-02-22 20:21:30 +0000

    this is a comment

---
+++ to_server_GIT.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#add all of the file into the repository
+git add .
+#commit to the local repository
+git commit -m "this is a comment"
+#push into the server
+git push -u origin master
+
